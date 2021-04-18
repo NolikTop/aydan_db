7735eb349bf7e829c88145bbb696ee25dbf3680c
NolikTop <aydansuper12@gmail.com>
2021-04-18 19:15:57 +0300

    feat(binary::Stream): сделал чтение/запись чисел ops: проверяю git hooks

--- binary/Stream.cpp
+++ binary/Stream.cpp
@@ -25,12 +25,54 @@ char *Stream::read(size_t len) const {
 }
 
 void Stream::write(size_t dataSize, char* data) {
-	writeNoRelease(dataSize, data);
+	writeNotReleaseValue(dataSize, data);
 	delete[] data;
 }
 
-void Stream::writeNoRelease(size_t dataSize, char *data) {
-	const size_t needSize = offset + dataSize;
+void Stream::writeNotReleaseValue(size_t valueSize, char *value) {
+	checkForWrite(valueSize);
+
+	memcpy(buffer + offset, value, valueSize);
+
+	offset += valueSize;
+}
+
+void Stream::writeNumber(size_t valueSize, int64_t value) {
+	if(valueSize > 8){
+		throw binary::Exception("Wrong valueSize");
+	}
+
+	checkForWrite(valueSize);
+
+	char* buf = buffer + offset;
+
+	// > 0, а не >= 0, потому что иначе происходит integer overflow
+	for(size_t j = valueSize - 1, i = j * 8; i > 0; i -= 8, j--){
+		buf[j] = (char)((value >> i) & 0xFF);
+	}
+
+	buf[0] = (char)(value & 0xFF);
+}
+
+int64_t Stream::readNumber(size_t valueSize) const {
+	if(valueSize > 8){
+		throw binary::Exception("Wrong valueSize");
+	}
+
+	auto bytes = read(valueSize);
+
+	int64_t num = 0;
+
+	// Странный момент. Ввел дополнительный j, чтобы лишний раз i не делить на 8
+	for(size_t i = 0, j = 0; i < valueSize; i += 8, j++) {
+		num |= ((int64_t)bytes[j]) << i;
+	}
+
+	return num;
+}
+
+void Stream::checkForWrite(size_t valueSize) {
+	const size_t needSize = offset + valueSize;
 	size_t newCap;
 	if(needSize > cap){
 		if(needSize >= CAP_X2){
@@ -47,38 +89,22 @@ void Stream::writeNoRelease(size_t dataSize, char *data) {
 
 		cap = newCap;
 	}
-
-	memcpy(buffer + offset, data, dataSize);
-
-	offset += dataSize;
 }
-
-
-void Stream::writeNumber(size_t size, int64_t value) {
-
-}
-
-int64_t Stream::readNumber(size_t size) const {
-	return 0;
-}
-
 
 void Stream::writeSignedByte(int8_t value) {
-	write(sizeof(char), new char[1]{
-		(char)value
-	});
+	writeNumber(sizeof(int8_t), value);
 }
 
 int8_t Stream::readSignedByte() const {
-	return read(1)[0];
+	return (int8_t)readNumber(sizeof(int8_t));
 }
 
 void Stream::writeUnsignedByte(u_int8_t value) {
-	writeSignedByte((int8_t) value);
+	writeNumber(sizeof(int8_t), value);
 }
 
 u_int8_t Stream::readUnsignedByte() const {
-	return readSignedByte();
+	return (int8_t)readNumber(sizeof(int8_t));
 }
 
 void Stream::writeSignedShort(int16_t value) {
@@ -101,30 +127,30 @@ void Stream::writeSignedInt32(int32_t value) {
 	writeNumber(sizeof(int32_t), value);
 }
 
-u_int32_t Stream::readSignedInt32() {
-	return 0;
+u_int32_t Stream::readSignedInt32() const {
+	return (int32_t) readNumber(sizeof(int32_t));
 }
 
 void Stream::writeUnsignedInt32(u_int32_t value) {
-
+	writeNumber(sizeof(u_int32_t), value);
 }
 
-u_int32_t Stream::readUnsignedInt32() {
-	return 0;
+u_int32_t Stream::readUnsignedInt32() const {
+	return (u_int32_t) readNumber(sizeof(u_int32_t));
 }
 
 void Stream::writeSignedLong(int64_t value) {
-
+	writeNumber(sizeof(int64_t), value);
 }
 
-int64_t Stream::readSignedLong() {
-	return 0;
+int64_t Stream::readSignedLong() const {
+	return readNumber(sizeof(int64_t));
 }
 
 void Stream::writeUnsignedLong(u_int64_t value) {
-
+	writeNumber(sizeof(u_int64_t), (int64_t) value);
 }
 
-u_int64_t Stream::readUnsignedLong() {
-	return 0;
+u_int64_t Stream::readUnsignedLong() const {
+	return readNumber(sizeof(u_int64_t));
 }
--- binary/Stream.h
+++ binary/Stream.h
@@ -24,16 +24,18 @@ namespace binary {
 		[[nodiscard]] inline char *read(size_t len) const;
 
 		inline void write(size_t dataSize, char* data);
-		inline void writeNoRelease(size_t dataSize, char* data);
+		inline void writeNotReleaseValue(size_t valueSize, char* value);
 
-		inline void writeNumber(size_t size, int64_t value);
-		[[nodiscard]] inline int64_t readNumber(size_t size) const;
+		inline void writeNumber(size_t valueSize, int64_t value);
+		[[nodiscard]] int64_t inline readNumber(size_t valueSize) const;
+
+		inline void checkForWrite(size_t valueSize);
 
 		inline void writeSignedByte(int8_t value);
-		[[nodiscard]] inline int8_t readSignedByte() const;
+		[[nodiscard]] int8_t inline readSignedByte() const;
 
 		inline void writeUnsignedByte(u_int8_t value);
-		[[nodiscard]] inline u_int8_t readUnsignedByte() const;
+		[[nodiscard]] u_int8_t inline readUnsignedByte() const;
 
 		inline void writeSignedShort(int16_t value);
 		[[nodiscard]] int16_t inline readSignedShort() const;
@@ -42,16 +44,16 @@ namespace binary {
 		[[nodiscard]] u_int16_t inline readUnsignedShort() const;
 
 		inline void writeSignedInt32(int32_t value);
-		u_int32_t inline readSignedInt32();
+		[[nodiscard]] u_int32_t inline readSignedInt32() const;
 
 		inline void writeUnsignedInt32(u_int32_t value);
-		u_int32_t inline readUnsignedInt32();
+		[[nodiscard]] u_int32_t inline readUnsignedInt32() const;
 
 		inline void writeSignedLong(int64_t value);
-		int64_t inline readSignedLong();
+		[[nodiscard]] int64_t inline readSignedLong() const;
 
 		inline void writeUnsignedLong(u_int64_t value);
-		u_int64_t inline readUnsignedLong();
+		[[nodiscard]] u_int64_t inline readUnsignedLong() const;
 
 	};
 
